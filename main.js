f84974df83c0e5ea60c39db5e70f2cdca79c8cf3
Sinan Pasic <sinan.p.pasic@gmail.com>
2023-03-29 09:29:49 +0200

    new

---
+++ main.js
@@ -0,0 +1,17 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+const SIZE = 1000;
+const TAU = 2 * Math.PI;o
+
+let offsets;
+
+function init_canvas() {
+    canvas.width = SIZE;
+    canvas.height = SIZE;
+    ctx.fillStyle = "White";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function draw_numberline
+
+init_canvas()
